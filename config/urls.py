db989aff1893a9704d611530ba03287a350f5d05
Rishat <rishgall06@gmail.com>
2026-05-19 13:15:55 +0500

    drf setup

--- config/urls.py
+++ config/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
+    # Apps API
+    path('api/applications/', include('applications.urls')),
     path('api/news/', include('news.urls')),
 ]
