2a2aaf15459704ad818ae7579655f044ba604a2d
Rishat <rishgall06@gmail.com>
2026-05-21 11:14:46 +0500

    news is set up

--- apps/news/models.py
+++ apps/news/models.py
@@ -1,23 +1,20 @@
+import uuid
 from django.db import models
-from django.conf import settings
+from core.models import User
 
 class News(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255, verbose_name="Заголовок")
-    content = models.TextField(verbose_name="Содержание")
-    image = models.ImageField(upload_to='news_images/', blank=True, null=True, verbose_name="Изображение")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.SET_NULL, 
-        null=True, 
-        verbose_name="Автор"
-    )
+    content = models.TextField(verbose_name="Текст новости")
+    image = models.ImageField(upload_to='news/%Y/%m/', blank=True, null=True, verbose_name="Изображение")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name="Автор")
     is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
 
     class Meta:
         verbose_name = "Новость"
         verbose_name_plural = "Новости"
-        ordering = ['-created_at'] # Сначала новые
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
--- apps/news/views.py
+++ apps/news/views.py
@@ -1,9 +1,15 @@
-from rest_framework import generics, permissions
-from .models import News    
+from rest_framework import viewsets, permissions
+from .models import News
 from .serializers import NewsSerializer
 
-class NewsListView(generics.ListAPIView):
+
+class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.filter(is_published=True)
     serializer_class = NewsSerializer
-    # Вот здесь мы ограничиваем доступ:
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [
+                permissions.AllowAny()
+            ]  # Новости могут видеть даже неавторизованные (по желанию)
+        return [permissions.IsAdminUser()]  # Создавать может только персонал
